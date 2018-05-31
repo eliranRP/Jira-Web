706752700892f26c33666f4e3eea9840f731fcf8
Eliran <Eliran@moveo.co.il>
2018-05-31 10:48:50 +0300

    no message

---
+++ js/controllers/loadData.js
@@ -0,0 +1,16 @@
+﻿MainApp.controller('LoadData', ['$scope', 'rx', 'observeOnScope', 'utils', 'sprintDbController', 'issueDbController',
+    'userDbController','graphService',
+    function ($scope, rx, observeOnScope, utils, sprintDbController, issueDbController,
+        userDbController, graphService) {
+
+
+
+
+        issueDbController.uploadSprint("../../../sprint_12_399.json", "399")
+           .subscribe(function (results) {
+               console.log("results: ", results)
+           }, function (e) {
+               console.log("error: ", e)
+           });
+
+    }]);
