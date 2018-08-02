f681f34486e790c50a34f4feb70c51c925950359
Eliran <Eliran@moveo.co.il>
2018-08-02 09:30:30 +0300

    Adding more sprints

--- js/controllers/loadData.js
+++ js/controllers/loadData.js
@@ -3,9 +3,8 @@
     function ($scope, rx, observeOnScope, utils, sprintDbController, issueDbController,
         userDbController, graphService, projectDbController) {
 
-        var sprints = [
-            {
-                path: "../../../sprint_16_417.json",
+        var sprints = [{
+                path: "../../sprint_16_417.json",
                 id: "417",
                 name: "Sprint 16"
             },
@@ -33,53 +32,86 @@
                 path: "../../../sprint_21_439.json",
                 id: "439",
                 name: "Sprint 21"
+            },
+            {
+                path: "../../../sprint_22_443.json",
+                id: "443",
+                name: "Sprint 22"
+            },
+            {
+                path: "../../../sprint_23_448.json",
+                id: "448",
+                name: "Sprint 23"
+            },
+            {
+                path: "../../../sprint_24_454.json",
+                id: "454",
+                name: "Sprint 24"
+            },
+            {
+                path: "../../../sprint_25_456.json",
+                id: "456",
+                name: "Sprint 25"
+            },
+            {
+                path: "../../sprint_26_460.json",
+                id: "460",
+                name: "Sprint 26"
+            },
+            {
+                path: "../../../sprint_27_470.json",
+                id: "470",
+                name: "Sprint 27"
             }
         ];
 
-        console.table(sprints, ['path', 'id', 'name'])
-        console.log("hii there")
+        // console.table(sprints, ['path', 'id', 'name'])
+        // console.log("hii there")
 
 
-        // // //Load issues
+        //#####  Load first(1)  ####
+
+        //Load issues
         // Rx.Observable.from(sprints)
-        //    .flatMap(sprint => {
-        //        return issueDbController.uploadSprint(sprint.path, sprint.id, sprint.name)
-        //    })
-        //    .subscribe(function (results) {
-        //        console.log("results: ", results)
-        //    }, function (e) {
-        //        console.log("error: ", e)
-        //    });
+        //     .flatMap(sprint => {
+        //         return issueDbController.uploadSprint(sprint.path, sprint.id, sprint.name)
+        //     })
+        //     .subscribe(function (results) {
+        //         console.log("results: ", results)
+        //     }, function (e) {
+        //         console.log("error: ", e)
+        //     });
 
 
 
-        //    //Load sprints
 
-        //     sprintDbController.uploadList("../../../sprint_list.json")
-        //         .subscribe(function (results) {
-        //             console.log("results: ", results)
-        //         }, function (e) {
-        //             console.log("error: ", e)
-        //          });
+        // //Load sprints
 
-
-        //    // Load projects
-        //     projectDbController.uploadList("../../../projects.json")
-        //        .subscribe(function (results) {
-        //            console.log("results: ", results)
-        //        }, function (e) {
-        //            console.log("error: ", e)
-        //        });
+        // sprintDbController.uploadList("../../../sprint_list.json")
+        //     .subscribe(function (results) {
+        //         console.log("results: ", results)
+        //     }, function (e) {
+        //         console.log("error: ", e)
+        //     });
 
 
+        // // Load projects
+        // projectDbController.uploadList("../../../projects.json")
+        //     .subscribe(function (results) {
+        //         console.log("results: ", results)
+        //     }, function (e) {
+        //         console.log("error: ", e)
+        //     });
 
-        //     //Load users
-        //    userDbController.createListFrom("issue")
-        //        .subscribe(function (results) {
-        //            console.log("results: ", results)
-        //        }, function (e) {
-        //            console.log("error: ", e)
-        //        });
+
+        //#####  Load after(2)  ####
+        //Load users
+        // userDbController.createListFrom("issue")
+        //     .subscribe(function (results) {
+        //         console.log("results: ", results)
+        //     }, function (e) {
+        //         console.log("error: ", e)
+        //     });
 
 
     }
--- js/controllers/testController.js
+++ js/controllers/testController.js
@@ -1,27 +1,77 @@
-﻿MainApp.controller('testCtrl', ['$scope', 'rx', 'observeOnScope', 'utils', 'sprintDbController', 'issueDbController',
+﻿
+MainApp.controller('testCtrl', ['$scope', 'rx', 'observeOnScope', 'utils', 'sprintDbController', 'issueDbController',
     'userDbController', 'graphService', 'projectDbController',
     function ($scope, rx, observeOnScope, utils, sprintDbController, issueDbController,
         userDbController, graphService, projectDbController) {
 
 
-        var projectId = '10104';
-        // var group = [{id: "eliran"}, {id: "tavor cohen"},{id: "alon"},{id: "elad"},{id: "maorb"},{id: "matan"},{id: "tamara"}]
-        var group = [{id: "eliran"}]
-        $scope.totalPointsInProject = 0
+        // var projectId = '10104';
+        // // var group = [{id: "eliran"}, {id: "tavor cohen"},{id: "alon"},{id: "elad"},{id: "maorb"},{id: "matan"},{id: "tamara"}]
+        // var group = [{
+        //     id: "eliran"
+        // }]
+        // $scope.totalPointsInProject = 0
 
 
 
-        var barChart;
+        // var barChart;
 
-        //Load projects
-        issueDbController.getPointsAfterSprintStarts(group)
-            .subscribe(function (results) {
-                    console.log("projects: ", results)
-                },
-                function (e) {
-                    console.log("error: ", e)
-                });
+        // //Load projects
+        // issueDbController.getPointsAfterSprintStarts(group)
+        //     .subscribe(function (results) {
+        //             console.log("projects: ", results)
+        //         },
+        //         function (e) {
+        //             console.log("error: ", e)
+        //         });
 
 
+
+
+        var times = [{
+                value: 0,
+                time: 100
+            },
+            {
+                value: 1,
+                time: 600
+            },
+            {
+                value: 2,
+                time: 400
+            },
+            {
+                value: 3,
+                time: 900
+            },
+            {
+                value: 4,
+                time: 200
+            }
+        ];
+
+        // Delay each item by time and project value;
+        var source = Rx.Observable.from(times)
+            .flatMap(function (item) {
+                return Rx.Observable
+                    .of(item.value)
+                    .delay(item.time);
+            })
+            // .throttleFirst(3000 /* ms */ );
+
+        var subscription = source.subscribe(
+            function (x) {
+                console.log('Next: %s', x);
+            },
+            function (err) {
+                console.log('Error: %s', err);
+            },
+            function () {
+                console.log('Completed');
+            });
     }
+
+
+
+
 ]);
